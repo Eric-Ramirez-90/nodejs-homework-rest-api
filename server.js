84f18985e8d8ead779b287d8fd61d3c486c36dc0
Eric Ramirez <ramirez1990rer@gmail.com>
2023-04-15 00:11:21 +0300

    added .env

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB_URI =
-  'mongodb+srv://Eric:Q3zZfcToRMPrcfu0@cluster0.m5tfpto.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const { DB_URI, PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', true);
 
 mongoose
   .connect(DB_URI)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log('Database connection successful');
   })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log('Server running. Use our API on port: 3000');
-// });
