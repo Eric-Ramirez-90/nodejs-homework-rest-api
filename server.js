58fa583d72b4c6e290ee0b2efc4da1455e3b1e79
Eric Ramirez <ramirez1990rer@gmail.com>
2023-04-14 23:02:25 +0300

    added connect with MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const DB_URI =
+  'mongodb+srv://Eric:Q3zZfcToRMPrcfu0@cluster0.m5tfpto.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    app.listen(3000);
+    console.log('Database connection successful');
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log('Server running. Use our API on port: 3000');
+// });
