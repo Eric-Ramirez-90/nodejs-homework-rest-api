3b6e16f606dc40d9756d00458573ef354dfe635e
Eric Ramirez <ramirez1990rer@gmail.com>
2023-04-23 19:15:17 +0300

    added User schema

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 
+const Joi = require('joi');
+
 const { handleMongooseError } = require('../helpers');
 
 const contactSchema = new Schema(
@@ -24,6 +26,43 @@ const contactSchema = new Schema(
 
 contactSchema.post('save', handleMongooseError);
 
+const addSchema = Joi.object({
+  name: Joi.string().min(5).max(15).trim().required(),
+  email: Joi.string().email().trim().required(),
+  phone: Joi.string()
+    .min(10)
+    .max(15)
+    .regex(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/)
+    .trim()
+    .required(),
+  favorite: Joi.boolean(),
+});
+
+const updateSchema = Joi.object({
+  name: Joi.string().min(5).max(15).trim().optional(),
+  email: Joi.string().email().trim().optional(),
+  phone: Joi.string()
+    .min(10)
+    .max(15)
+    .regex(/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/)
+    .trim()
+    .optional(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateSchema,
+  updateFavoriteSchema,
+};
+
 const Contact = model('contact', contactSchema);
 
-module.exports = Contact;
+module.exports = {
+  Contact,
+  schemas,
+};
